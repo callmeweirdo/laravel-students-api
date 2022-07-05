6743eb5173a80ff69e5c4d10b2c991a86f83d8d8
Realest Weirdo <ogbandav102@gmail.com>
2022-07-05 21:33:57 +0100

    xo i added endpoints like edit-students, update and delete students as well bringing me to the conclution of this mini api projects and all of its end points

--- app/Http/Controllers/API/StudentsController.php
+++ app/Http/Controllers/API/StudentsController.php
@@ -5,30 +5,108 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentsController extends Controller
 {
+
+    public function index(){
+        $students = Student::all();
+        // $students = [];
+        return response()->json([
+            'status' => 200,
+            'students' => $students,
+            'message' => 'Successe'
+        ]);
+    }
+
     public function store(Request $request){
-        $data = [
-            'name' => $request->name,
-            'course' => $request->course,
-            'email' => $request->email,
-            'phone' => $request->phone
-        ];
 
-        $students = Student::create($data);
+        $validator = Validator::make($request->all(), [
+            "name" => ['required', 'max:191'],
+            "course" => ['required', 'max:191'],
+            "email" => ['required', 'max:191', 'email'],
+            "phone" => ['required', 'max:10', 'min:10']
+        ]);
 
-        if($students){
+        // $data = [
+        //     'name' => $request->name,
+        //     'course' => $request->course,
+        //     'email' => $request->email,
+        //     'phone' => $request->phone
+        // ];
+
+        if($validator->fails()){
             return response()->json([
-                'staus' => 200,
-                'message' => 'Students Added Succesfully'
+                'errors' => $validator->errors()
+            ]);
+        }else{
+            $students = Student::create($validator->validated());
+            if($students){
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Students Added Succesfully'
+
+                ]);
+            }
+        }
+    }
+
+    public function edit( $id){
+        $student = Student::find($id);
+
+        if($student){
+            return response()->json([
+                "status" => 200,
+                "student" => $student,
             ]);
         }else{
             return response()->json([
-                'status' => 404,
-                'message' => 'something is wrong'
+                "status" => 404,
+                "message" => "Page Not Found"
             ]);
         }
+        // dd($request);
+    }
 
+    public function update($id, Request $request){
+        $validator = Validator::make($request->all(), [
+            "name" => ['required', 'max:191'],
+            "course" => ['required', 'max:191'],
+            "email" => ['required', 'max:191', 'email'],
+            "phone" => ['required', 'max:10', 'min:10']
+        ]);
+
+
+        if($validator->fails()){
+            return response()->json([
+                'errors' => $validator->errors()
+            ]);
+        }else{
+            $student = Student::find($id);
+            if($student){
+                $student->create($validator->validated());
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Students Added Succesfully'
+
+                ]);
+            }else{
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Students Added Succesfully'
+                ]);
+            }
+        }
+    }
+
+    public function destroy($id){
+        $student = Student::find($id);
+        $student->delete();
+
+        return response()->json([
+            "status" => 200,
+            "message" => "deleted Successfully"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\StudentsController;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::delete('/delete-student/{id}', [StudentsController::class, "destroy"]);
+Route::put('/update-student/{id}', [StudentsController::class, "update"]);
+Route::get('/edit-student/{id}', [StudentsController::class, 'edit']);
+Route::get('/students', [StudentsController::class, 'index']);
 Route::post('/add-student', [StudentsController::class, 'store']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
